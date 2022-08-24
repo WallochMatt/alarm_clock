468e5e869f780fe051a856760b6e6eb10e886d27
WallochMatt <matthewrwalloch@gmail.com>
2022-08-24 13:56:22 -0500

    feat: Created class and methods for alarm clocks

--- clock_class.py
+++ clock_class.py
@@ -1,21 +1,22 @@
 class Alarm:
-    def __init__(self, time):
+    def __init__(self, time, set_to):
         self.display_time = time
         self.is_on = False #use switch method to toggle to ture
-        self.time_set_to = "5:00"
-        pass
+        self.time_set_to = set_to
 
     def set_display_time(self):#parameter may be needed
+        # time = input("Set the time: ")
+        # print(time)
         self.display_time = input("Set the time: ")
         print(self.display_time)
 
-    def switch(self):
-        if self.is_on == False:
-            choice = int(input("Turn on?: \n ON: 1 \n OFF: 2"))
-            if choice == 1: 
-                self.is_on = True
-            elif choice == 2:
-                self.is_on = False
+    def switch(self):  #self.is_on showed as True in debugger stepping, works
+        choice = int(input("Would you like the alarm clock on? \n ON: 1 \n OFF: 2 \n"))
+        if choice == 1:#ON
+            self.is_on = True
+        if choice == 2:#OFF
+            self.is_on = False
+ 
 
 
     def current_alarm_time(self):
@@ -23,3 +24,5 @@ class Alarm:
         print(self.time_set_to)
 
 
+    def show_me_time(self):
+        print(self.display_time)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from clock_class import Alarm
 
 
-new_alarm_clock = Alarm("1:10")
+new_alarm_clock = Alarm("1:00", "3:45")
+
+#new_alarm_clock.set_display_time()
+
+#new_alarm_clock.switch()
+
+#new_alarm_clock.current_alarm_time()
+
+new_alarm_clock.show_me_time()
